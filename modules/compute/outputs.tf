2d227b284910ff663c7776dd6fb3308f674a524c
Nithin <168177111+nithingowdahm87@users.noreply.github.com>
2026-06-02 13:15:51 +0530

    feat: workspace isolation, org SCPs, compliance-as-code, secrets rotation lambda, service quota alarms, secondary ASGs, KMS CMK, S3 account-level block, dependabot automation

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,2 +1,9 @@
-output "asg_name" { value = aws_autoscaling_group.this.name }
-output "launch_template_id" { value = aws_launch_template.this.id }
+output "asg_name" {
+  description = "Auto Scaling Group name"
+  value       = aws_autoscaling_group.this.name
+}
+
+output "launch_template_id" {
+  description = "Launch template ID"
+  value       = aws_launch_template.this.id
+}
