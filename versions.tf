a83b1b9c585dd4869d50d1bf155ea63b22f9e595
Nithin <168177111+nithingowdahm87@users.noreply.github.com>
2026-06-02 14:50:28 +0530

    chore: pin provider versions to prevent silent breakage on new releases

--- versions.tf
+++ versions.tf
@@ -4,11 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.50"
     }
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
     }
   }
 
