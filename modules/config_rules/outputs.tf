328ef18bbe1d1da52e3b4921cc983c13c05a0167
Nithin <168177111+nithingowdahm87@users.noreply.github.com>
2026-06-09 11:35:23 +0530

    fix: add missing recorder_id output required by compliance module

--- modules/config_rules/outputs.tf
+++ modules/config_rules/outputs.tf
@@ -1,2 +1,3 @@
 output "recorder_name"  { value = aws_config_configuration_recorder.this.name }
+output "recorder_id"    { value = aws_config_configuration_recorder.this.id }
 output "config_bucket"  { value = aws_s3_bucket.config.bucket }
