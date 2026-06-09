d34a7829d932a0b264bc4dc5bb2d72eb14975791
Nithin <168177111+nithingowdahm87@users.noreply.github.com>
2026-06-09 11:34:11 +0530

    fix: add missing primary_cluster_id and secondary_cluster_id outputs

--- modules/aurora_global/outputs.tf
+++ modules/aurora_global/outputs.tf
@@ -1,7 +1,9 @@
-output "global_cluster_id" { value = aws_rds_global_cluster.this.id }
-output "primary_cluster_endpoint" { value = aws_rds_cluster.primary.endpoint }
+output "global_cluster_id"        { value = aws_rds_global_cluster.this.id }
+output "primary_cluster_endpoint"  { value = aws_rds_cluster.primary.endpoint }
 output "secondary_cluster_endpoint" { value = aws_rds_cluster.secondary.endpoint }
-output "primary_reader_endpoint" { value = aws_rds_cluster.primary.reader_endpoint }
-# FIX: expose cluster ARN for use in backup module resource_arns
-output "primary_cluster_arn" { value = aws_rds_cluster.primary.arn }
-output "secondary_cluster_arn" { value = aws_rds_cluster.secondary.arn }
+output "primary_reader_endpoint"   { value = aws_rds_cluster.primary.reader_endpoint }
+output "primary_cluster_arn"       { value = aws_rds_cluster.primary.arn }
+output "secondary_cluster_arn"     { value = aws_rds_cluster.secondary.arn }
+# Required by module.observability and module.alerting in root main.tf
+output "primary_cluster_id"        { value = aws_rds_cluster.primary.cluster_identifier }
+output "secondary_cluster_id"      { value = aws_rds_cluster.secondary.cluster_identifier }
